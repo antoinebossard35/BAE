5ef7cc0ccdbc01ee700ce990ec5aaf8d64e3594a
antoinebossard35 <33938834+antoinebossard35@users.noreply.github.com>
2018-01-18 17:49:14 +0100

    new commit for develop_antoine branch

---
+++ app/src/main/java/com/decathlon/bae/bae/AreaInformationActivity.java
@@ -0,0 +1,17 @@
+package com.decathlon.bae.bae;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Antoine on 18/01/2018.
+ */
+
+public class AreaInformationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_area);
+    }
+
+}
---
+++ app/src/main/java/com/decathlon/bae/bae/LevelInformationActivity.java
@@ -0,0 +1,18 @@
+package com.decathlon.bae.bae;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Antoine on 18/01/2018.
+ */
+
+public class LevelInformationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_level);
+    }
+
+}
